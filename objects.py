b1b4a57f729be186f0d826a6e104e883051d1091
Hongfei Tian <hongfei.tian@ey.com>
2018-03-06 14:03:34 -0500

    basic objects definitions

---
+++ objects.py
@@ -0,0 +1,441 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 10:15:29 2018
+
+@author: PG738LD
+"""
+
+"""############################################################
+   # Card     Set              Partial                        #
+   #          |-- Pair         |-- Dandiao                    #
+   #          |-- Shun         |-- PaShun                     #
+   #          |-- YiErSan      |-- PaMixed                    #
+   #          |-- Mixed        |-- Pa2710                     #
+   #          |-- ErQiShi      |-- Liangjia                   #
+   #          |-- Ke                                          #
+   #          |   |-- Xiao                                    #
+   #          |   |-- Beng                                    #
+   #          |-- Gang                                        #
+   #              |-- Dia                                     #
+   #              |-- Pao                                     #
+   ############################################################
+"""
+
+from utils import HANZI
+
+
+class Card:
+    """A card is defined by 2 features, its number and type.
+    
+    Parameters
+    ----------
+    order : int
+        An integer in range(1, 11) or range(101, 111).
+    
+    Attributes
+    ----------
+    num : int
+        The playing number of this card.
+    tp : boolean
+        True means capital card; False means little card.
+    red : boolean
+        Card is defined to be red if its number is either 2, 7, or 10, 
+        otherwise it is black.
+    hanzi : str
+        The number of this card appeared on the real game card.
+    """
+    
+    def __init__(self, order):
+        self.order = order
+        self.num = self.order % 100
+        self.tp = bool(self.order // 100)
+        self.red = True if self.num in [2, 7, 10] else False
+        self.hanzi = HANZI[self.order]
+        
+    def __str__(self):
+        return self.hanzi
+    
+    def __lt__(self, other):
+        return self.order < other.order
+
+
+
+class Set:
+    """A (valid) Set is a group of 2, 3, or 4 cards that is allowed to appear
+    (as a group) in a winning hand. Types of Sets are Pair, Shun(normal, 123),
+    Mixed, 2710, Ke(Xiao, Beng), Gang(Dia, Pao).    
+    
+    Parameters
+    ----------
+    orders : list[int]
+        Orders to initialize cards
+    qia : int, Optional (default == -1)
+        1,2,3,4 for qia-position, 0 for not-qia, -1 for unknown
+    
+    Attributes
+    ----------
+    cards : list[Card]
+    length : int
+    red : int
+        Number of red cards
+    points : int
+    """
+    
+    def __init__(self, orders, qia=-1):
+        self.orders = sorted(orders)
+        self.qia = qia
+        self.cards = [Card(x) for x in self.orders]
+        self.length = len(self.orders)
+        self.red = len([c for c in self.cards if c.red])
+        self.points = 0
+    
+    def __str__(self):
+        if self.qia < 1:
+            return ''.join(['['] + [x.__str__() for x in self.cards] + [']'])
+        else:
+            return ''.join(
+                    ['['] + 
+                    [self.cards[i].__str__() for i in range(self.length) if i+1 != self.qia] + 
+                    ['+'] + 
+                    [self.cards[self.qia-1].__str__()] + 
+                    [']']
+                    )
+
+
+class Pair(Set):
+    """Just a pair.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this pairs
+    qia : int, Optional (default == -1)
+        2 for diandiao, 0 for zimo, -1 for unknown
+    """
+    def __init__(self, order, qia=-1):
+        super().__init__([order]*2, qia)
+
+
+class Shun(Set):
+    """A length 3 set that contains consecutive numbers from 234 to 8910. 
+    
+    Parameters
+    ----------
+    mid : int
+        The midder order of this Shun, from 3 to 9, or 103 to 109
+    qia : int, Optional (default == -1)
+        1,2,3 for qia-position, 0 for not-qia, -1 for unknown
+    """
+    def __init__(self, mid, qia=-1):
+        super().__init__([mid-1, mid, mid+1], qia)
+
+
+class YiErSan(Shun):
+    """A special Shun [1,2,3].
+    
+    Parameters
+    ----------
+    tp : boolean
+        True means capital 123, False means little 123.
+    qia : int, Optional (default == -1)
+        1,2,3 for qia-position, 0 for not-qia, -1 for unknown
+    """
+    def __init__(self, tp, qia=-1):
+        super().__init__(2+100*tp, qia)
+        self.points = 3 + 3*tp
+
+
+class Mixed(Set):
+    """A length 3 set that contains a single number but mixed types.
+    
+    Parameters
+    ----------
+    num : int
+        The number of this Mixed, from 1 to 10
+    two_caps : boolean
+        The number of capitals, True for 2, False for 1
+    qia : int, Optional (default == -1)
+        1,3 for qia-position, 0 for not-qia, -1 for unknown
+    """
+    def __init__(self, num, two_caps, qia=-1):
+        super().__init__([num, num+100*two_caps, num+100], qia)
+
+
+class ErQiShi(Set):
+    """A special Set [2, 7, 10].
+    
+    Parameters
+    ----------
+    tp : boolean
+        True means capital 2710, False means little 2710.
+    qia : int, Optional (default == -1)
+        1,2,3 for qia-position, 0 for not-qia, -1 for unknown
+    """
+    def __init__(self, tp, qia=-1):
+        super().__init__([x+100*tp for x in [2, 7, 10]], qia)
+        self.points = 3 + 3*tp
+
+
+class Ke(Set):
+    """A Set that contains 3 identical cards.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Ke.
+    qia : int, Optional (default == -1)
+        3 for beng, 0 for xiao, -1 for unknown
+    """
+    def __init__(self, order, qia=-1):
+        super().__init__([order]*3, qia)
+    
+    def gang(self):
+        return Gang(self.orders[0])
+        
+
+class Xiao(Ke):
+    """A special Ke that qia == 0.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Xiao.
+    """
+    def __init__(self, order):
+        super().__init__(order, 0)
+        self.points = 3 + 3*(order//100)
+    
+    def gang(self, zimo=False):
+        if zimo:
+            return Dia(self.orders[0])
+        else:
+            return Pao(self.orders[0], 1)
+        
+    def dia(self):
+        return self.gang(True)
+    
+    def pao(self):
+        return self.gang()
+    
+
+class Beng(Ke):
+    """A special Ke that qia > 0.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Beng
+    """
+    def __init__(self, order):
+        super().__init__(order, 3)
+        self.points = 1 + 2*(order//100)
+    
+    def gang(self, zimo=False):
+        return Pao(self.orders[0], 2-1*zimo)
+    
+    def pao(self):
+        return self.gang()
+
+
+class Gang(Set):
+    """A Set that contains 4 identical cards.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Gang
+    qia : int, Optional (default == -1)
+        3, 4 for pao, 0 for dia, -1 for unknown
+    """
+    def __init__(self, order, qia=-1):
+        super().__init__([order]*4, qia)
+
+
+class Dia(Gang):
+    """A special Gang that qia == 0
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Dia
+    """
+    def __init__(self, order):
+        super().__init__(order, 0)
+        self.points = 9 + 3*(order//100)
+
+
+class Pao(Gang):
+    """A special Gang that qia > 0
+    
+    Parameters
+    ----------
+    order : int
+        The order of any card in this Dia.
+    num_zimo : int
+        Number of Zimo Card in this Pao, 1 or 2
+    """
+    def __init__(self, order, num_zimo):
+        super().__init__(order, 4-num_zimo)
+        self.points = 6 + 3*(order//100)
+    
+    def __str__(self):
+        if self.qia == 3:
+            return super().__str__()
+        else:
+            return ('[' + self.cards[0].__str__()*2 + '+' + 
+                      self.cards[0].__str__()*2 + ']')
+
+
+
+class Partial:
+    """A Partial is a group of 1 or 2 cards that is calling for a card to
+    become a Set. Types of Partials are Dandiao, Liangjia, PartialShun(normal, 123),
+    Mixed, Partial2710.    
+    
+    Parameters
+    ----------
+    orders : list[int]
+        Orders to initialize cards
+    
+    Attributes
+    ----------
+    cards : list[Card]
+    length : int
+    red : int
+        Number of red cards
+    
+    """
+    def __init__(self, orders):
+        self.orders = sorted(orders)
+        self.cards = [Card(x) for x in self.orders]
+        self.length = len(self.orders)
+        self.red = len([c for c in self.cards if c.red])
+    
+    def __str__(self):
+        return ''.join(['['] + [x.__str__() for x in self.cards] + [']'])
+    
+    def qia(self, last):
+        return Set(self.orders+[last], self.length+1)
+
+
+class Dandiao(Partial):
+    """Just a single card.
+    
+    Parameters
+    ----------
+    order : int
+        The order of this card
+    """    
+    def __init__(self, order):
+        super().__init__([order])
+    
+    def fu(self):
+        return Pair(self.orders[0], 2)
+        
+
+class Liangjia(Partial):
+    """Two identical cards holding in hand.
+    
+    Parameters
+    ----------
+    order : int
+        The order of any of this 2 cards
+    """  
+    def __init__(self, order):
+        super().__init__([order]*2)
+    
+    def xiao(self):
+        return Xiao(self.orders[0])
+    
+    def beng(self):
+        return Beng(self.orders[0])
+    
+    def qia(self):
+        return Mixed(self.cards[0].num, 
+                     self.cards[0].tp, 
+                     1 if self.cards[0].tp else 3)
+
+
+
+class PaShun(Partial):
+    """Two different cards holding in hand, waiting for the third.
+    
+    Parameters
+    ----------
+    orders : list[int]
+        The orders of the 2 cards of this PaShun
+    
+    Attributes
+    ----------
+    is_lian : boolean
+        True if 2 numbers are adjacent, False if separate
+    waiting_list : list[int]
+        The candidate numbers of the third
+    
+    """  
+    def __init__(self, orders):
+        super().__init__(orders)
+        self.is_lian = self.orders[1]-self.orders[0]==1
+        self.waiting_list = [self.orders[0]+1] if not self.is_lian else \
+        [x for x in [self.orders[0]-1, self.orders[1]+1] if x%100 in range(1,11)]
+
+    def qia(self):
+        if not self.is_lian:
+            return self.qia_ka()
+        elif len(self.waiting_list) == 1:
+            return self.qia_bian(self.waiting_list[0])
+        else:
+            third = input('which card to qia? '+str(self.waiting_list[0])+
+                          ' or '+str(self.waiting_list[1])+'\n')
+            return self.qia_bian(int(third))
+    
+    def qia_ka(self):
+        return Shun(self.cards[0].order+1, 2) if self.cards[0].num!=1 else \
+        YiErSan(self.cards[0].tp, 2)
+    
+    def qia_bian(self, third):
+        if third > self.cards[1].order:
+            return Shun(self.cards[1].order, 3) if third%100!=3 else \
+            YiErSan(self.cards[0].tp, 3)
+        else:
+            return Shun(self.cards[0].order, 1) if third%100!=1 else \
+            YiErSan(self.cards[0].tp, 1)
+
+
+class PaMixed(Partial):
+    """Two cards with same number but different types.
+    
+    Parameters
+    ----------
+    num : int
+        The number of this PaMixed
+    """
+    def __init__(self, num):
+        super().__init__([num, num+100])
+    
+    def qia(self, tp):
+        return Mixed(self.cards[0].num, tp, 2)
+
+
+
+class Pa2710(Partial):
+    """Two of 2, 7, and 10, waiting the third.
+    
+    Parameters
+    ----------
+    orders : list[int]
+        The orders of the 2 cards of this Partial
+    
+    Attributes
+    ----------
+    waiting_list : list[int]
+        The candidate number of the third
+    """
+    def __init__(self, orders):
+        super().__init__(orders)
+        self.waiting_list = [x+100*self.cards[0].tp for x in [2,7,10] if \
+                             x not in [n%100 for n in orders]]
+    
+    def qia(self):
+        return ErQiShi(self.cards[0].tp, (self.waiting_list[0] % 100)//5+1)
+
---
+++ test_unit.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  6 12:55:21 2018
+
+@author: PG738LD
+"""
+
+import unittest
+import objects as obj
+
+
+class TestCardMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.da2 = obj.Card(102)
+        self.xiao6 = obj.Card(6)
+        self.xiao10 = obj.Card(10)
+
+    def test_attributes(self):
+        self.assertEqual(self.da2.num, 2)
+        self.assertEqual(self.da2.tp, True)
+        self.assertEqual(self.da2.red, True)
+        self.assertEqual(self.xiao6.num, 6)
+        self.assertEqual(self.xiao6.tp, False)
+        self.assertEqual(self.xiao6.red, False)
+
+    def test_str(self):
+        self.assertEqual(self.da2.__str__(), '贰')
+        self.assertEqual(self.xiao10.__str__(), '十')
+
+    def test_lt(self):
+        ls = [self.da2, self.xiao10, self.xiao6]
+        self.assertEqual(sorted(ls), [self.xiao6, self.xiao10, self.da2])
+
+
+class TestPairMethods(unittest.TestCase):
+    pass
+class TestShunMethods(unittest.TestCase):
+    pass
+class TestYiErSanMethods(unittest.TestCase):
+    pass
+class TestMixedMethods(unittest.TestCase):
+    pass
+class TestErQiShiMethods(unittest.TestCase):
+    pass
+class TestXiaoMethods(unittest.TestCase):
+    pass
+class TestBengMethods(unittest.TestCase):
+    pass
+class TestDiaMethods(unittest.TestCase):
+    pass
+class TestPaoMethods(unittest.TestCase):
+    pass
+class TestDandiaoMethods(unittest.TestCase):
+    pass
+class TestPaShunMethods(unittest.TestCase):
+    pass
+class TestPaMixedMethods(unittest.TestCase):
+    pass
+class TestPa2710Methods(unittest.TestCase):
+    pass
+class TestLiangjiaMethods(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
